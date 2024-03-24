6d45be1c89444512ba76a6a33e2f90597cac468b
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-24 22:53:00 +0500

    feat: create project; set dependencies

---
+++ src/main/java/kz/runtime/spring_practice_2/SpringPractice2Application.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring_practice_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPractice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kz/runtime/spring_practice_2/SpringPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring_practice_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
